65ce8960fd0aa85addad454dbdc6e2ce64435089
Christian Halim <chris.richard.halim@gmail.com>
2023-10-03 01:24:38 +0700

    add practices, TestContains and TestSplit

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,11 +1,34 @@
 package iteration
 
-const repeatCount = 5
+import "strings"
 
-func Repeat(character string) string {
+func Repeat(character string, number int) string {
 	var repeated string
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < number; i++ {
 		repeated += character
 	}
 	return repeated
+}
+
+func Contains(haysack, needle string) bool {
+	return strings.Contains(haysack, needle)
+}
+
+func Split(s, separator string) []string {
+	return strings.Split(s, separator)
+}
+
+func isSlicesEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		// fmt.Printf("%s is same with %s\n", v, b[i])
+		if v != b[i] {
+			// fmt.Printf("%s not same %s\n", v, b[i])
+			return false
+		}
+	}
+
+	return true
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,12 +1,67 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestContains(t *testing.T) {
+	haysack := Contains("Sword Art Online", "Online")
+	needle := "Online"
+
+	if !haysack {
+		t.Errorf("The string does not containing %q", needle)
+	}
+}
+
+func TestSplit(t *testing.T) {
+	split := Split("Getsuyoubi,Kayoubi,Suiyoubi,Mokuyoubi,Kinyoubi,Doyoubi,Nichiyoubi", ",")
+
+	expected := make([]string, len(split))
+	expected[0] = "Getsuyoubi"
+	expected[1] = "Kayoubi"
+	expected[2] = "Suiyoubi"
+	expected[3] = "Mokuyoubi"
+	expected[4] = "Kinyoubi"
+	expected[5] = "Doyoubi"
+	expected[6] = "Nichiyoubi"
+
+	if !isSlicesEqual(split, expected) {
+		t.Errorf("String %q splitted is not the same as %q", &split, expected)
+	}
+}
+
+func TestIsSlicesEqual(t *testing.T) {
+	slice1 := make([]string, 2)
+	slice1[0] = "1"
+	slice1[1] = "2"
+
+	slice2 := make([]string, 2)
+	slice2[0] = "1"
+	slice2[1] = "2"
+
+	if !isSlicesEqual(slice1, slice2) {
+		t.Errorf("String slices %q is not the same as %q", &slice1, slice2)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
 	}
 }
