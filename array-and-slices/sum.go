d866ea367a13dd0a7063d17d1e266a810ea13617
Christian Halim <chris.richard.halim@gmail.com>
2023-10-04 01:25:54 +0700

    handle empty slices

--- array-and-slices/sum.go
+++ array-and-slices/sum.go
@@ -9,11 +9,35 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
+	var sums []int
+	// sums := make([]int, lengthOfNumbers)
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// func SumAll(numbersToSum ...[]int) []int {
+// 	lengthOfNumbers := len(numbersToSum)
+// 	sums := make([]int, lengthOfNumbers)
+
+// 	for i, numbers := range numbersToSum {
+// 		sums[i] = Sum(numbers)
+// 	}
+// 	return sums
+// }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return sums
 }
