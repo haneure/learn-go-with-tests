d2b3eb4441b01e3622a87f515181370040b57282
Christian Halim <chris.richard.halim@gmail.com>
2023-10-10 15:57:40 +0700

    add countdown

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,10 +1,34 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
+	"time"
 )
 
-func Countdown(out *bytes.Buffer) {
-	fmt.Fprint(out, "3")
+type Sleeper interface {
+	Sleep()
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
