1a74569b046136609f2d0950e1fde3e08827bb5a
Christian Halim <chris.halim@mobilepulsa.com>
2023-10-16 13:03:51 +0700

    add select

---
+++ reflection/walk.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			fmt.Println(v)
+			fmt.Println(ok)
+			walkValue(v)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
+}
+
+func main() {
+	var got []string
+
+	x := struct {
+		Name    string
+		Profile struct {
+			Age  int
+			City string
+		}
+	}{"Chris", struct {
+		Age  int
+		City string
+	}{23, "Indonesia"}}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+}
